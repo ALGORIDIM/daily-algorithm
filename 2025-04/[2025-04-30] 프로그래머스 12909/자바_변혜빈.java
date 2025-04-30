0121d6443fe4f1becb2a35cd3e64844d72032201
헤빈 <hyebeenbyeon@gmail.com>
2025-04-30 23:59:30 +0900

    자바_변혜빈(#27)

---
+++ 2025-04/[2025-04-30] 프로그래머스 12909/자바_변혜빈.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+class Solution {
+    boolean solution(String s) {
+        Stack<Integer> stack = new Stack<>();
+        for(int i =0; i < s.length(); i++) {
+            char tmp = s.charAt(i);
+            if (tmp == '(') {
+                stack.push(1);
+            } else {
+                if (stack.isEmpty())
+                    return false;
+                stack.pop();
+            }
+        }
+        return stack.isEmpty();
+    }
+}
