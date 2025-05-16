1461a9e02f89ab86c2079f842d6ce2c30ea5d8d2
Jeong Sujin <wjdtnwls0106@gmail.com>
2025-05-17 01:50:04 +0900

    자바_정수진(#33)

---
+++ 2025-05/[2025-05-16] 백준 2609/자바_정수진.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class 자바_정수진 {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        int gcd = gcd(a, b);
+        int lcm = lcm(a, b);
+
+        bw.write(String.valueOf(gcd));
+        bw.newLine();
+        bw.write(String.valueOf(lcm));
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+    static int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+
+    static int lcm(int a, int b) {
+        return (a * b) / gcd(a, b);
+    }
+}
